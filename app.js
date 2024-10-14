e16510ace469351b3c6d95b0427280ea6bc8d66b
Kin Lane <kinlane@gmail.com>
2024-10-14 19:48:18 -0400

    add path

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.use(cors());
 const getContracts = require('./paths/getContracts.js');
 app.use('/contracts', getContracts);
 
+const getContract = require('./paths/getContract.js');
+app.use('/contracts/:aid', getContract);
+
 app.listen(3300, () => {
   console.log('Server listening on port 3300');
 });
---
+++ paths/getContract.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mysql = require('mysql');
+const router = express.Router();
+const store = require('../../store/keys.json');
+
+var connection = mysql.createConnection({
+  host     : store.api_search_database_host,
+  database : store.api_search_database_database,
+  user: store.api_search_database_user,
+  password: store.api_search_database_password
+  });
+
+router.get('/', (req, resp)=>{ 
+
+  var aid = req.params.aid;
+
+  var contracts_sql = "SELECT * FROM contracts WHERE aid = '" + aid + "'";
+  connection.query(contracts_sql, function (error, contracts, fields) { 
+
+    var totalPages = 1;
+
+    var meta = {};
+    meta.limit = 1;
+    meta.page = 0;
+    meta.totalPages = 1;
+
+    var response = {};
+    response.meta = meta;
+    response.data = contracts;
+    
+    resp.send(response);    
+    
+  }).on('error', err => {
+    resp.send(err);
+  });                           
+
+})
+
+module.exports = router;
