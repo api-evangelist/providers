f9efaa59ac0dbd9fce31fcb37e7650152784a747
Kin Lane <kinlane@gmail.com>
2024-10-14 18:34:58 -0400

    update

--- paths/getContracts.js
+++ paths/getContracts.js
@@ -29,7 +29,7 @@ router.get('/', (req, resp)=>{
     }
   }
 
-  var count_sql = "SELECT COUNT(*) FROM contracts WHERE name IS NOT NULL";
+  var count_sql = "SELECT count(name) FROM contracts WHERE name IS NOT NULL";
   if(search!=''){
     count_sql += " AND (name LIKE '%" + search + "%' OR description LIKE '%" + search + "%' OR tags LIKE '%" + search + "%')";
   }
