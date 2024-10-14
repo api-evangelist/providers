d36ace27fdb5d0d69ca28d3e7351b3e8ffabfeb1
Kin Lane <kinlane@gmail.com>
2024-10-14 19:12:16 -0400

    update

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
 const getContracts = require('./paths/getContracts.js');
 app.use('/contracts', getContracts);
-
-app.use(cors({
-  origin: '*', // Allow requests from this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-  credentials: true // Allow credentials (cookies, authorization headers)
-}));
 
 app.listen(3300, () => {
   console.log('Server listening on port 3300');
