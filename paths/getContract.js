1c37476b6b1846ab99f691bd0061c1f7dd1d2d06
Kin Lane <kinlane@gmail.com>
2024-10-14 20:25:27 -0400

    update

--- paths/getContract.js
+++ paths/getContract.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
-const router = express.Router({ mergeParams: true });
+const router = express.Router();
 const store = require('../../store/keys.json');
 
 var connection = mysql.createConnection({
