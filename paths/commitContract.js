a784d04791a7342f0468af4b57ea162ccc9889d7
Kin Lane <kinlane@gmail.com>
2024-10-19 20:07:30 -0400

    update

--- paths/commitContract.js
+++ paths/commitContract.js
@@ -176,7 +176,7 @@ router.put('/', (req, resp)=>{
               
                                     // BEGIN COMMIT TO GITHUB
                                     const options = {
-                                        method: 'PUT',
+                                        method: 'POST',
                                         headers: {
                                             "Accept": "application/vnd.github+json",
                                             "X-GitHub-Api-Version": "2022-11-28",
