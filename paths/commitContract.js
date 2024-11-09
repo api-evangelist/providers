6dda1bd2163fa2c668f52d989c5c02a050aa8064
Kin Lane <kinlane@gmail.com>
2024-11-09 15:02:48 -0500

    ddd

--- paths/commitContract.js
+++ paths/commitContract.js
@@ -164,6 +164,8 @@ router.put('/', (req, resp)=>{
 
     var repo = contract[0].repo;
 
+    resp.send(repo);
+
     // BEGIN PULL FILE
     var changes_sql = "SELECT DISTINCT file FROM changes WHERE aid = '" + aid + "' AND committed = 0";
     connection.query(changes_sql, function (error, changes, fields) { 
@@ -187,8 +189,7 @@ router.put('/', (req, resp)=>{
         Bucket: bucket,
         Key: key, 
       };
-    
-      resp.send(params);
+        
       const streamToString = (stream) =>
         new Promise((resolve, reject) => {
           const chunks = [];
