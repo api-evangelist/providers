cf06e9cd04cd861e573a6448870ed7e386955043
Kin Lane <kinlane@gmail.com>
2024-11-14 20:00:35 -0500

    ddd

--- paths/getContracts.js
+++ paths/getContracts.js
@@ -66,7 +66,10 @@ router.get('/', (req, resp)=>{
   if(req.query.page){
     page = req.query.page;
     page = page - 0;
-    page2 = page + limit;
+    if(page > 0 ){ 
+      page = page + limit; 
+      limit = limit + limit;
+    }
   }
   else{
     page = 0;
