f1c379a24933ca336c435d8974f29e38da0a647b
Kin Lane <kinlane@gmail.com>
2024-12-04 17:01:22 -0500

    update

--- paths/getContracts.js
+++ paths/getContracts.js
@@ -91,7 +91,7 @@ router.get('/', (req, resp)=>{
     if(access && access.length > 1){
       contracts_sql += " AND access = '" + access + "'";
     }     
-    contracts_sql += " ORDER BY Name ASC";
+    contracts_sql += " ORDER BY Rand()";
 
     contracts_sql += " LIMIT " + page + "," + limit;
 
