6a89470f3f1dd415950f6c2cd720c639cedfda8c
Kin Lane <kinlane@gmail.com>
2025-01-02 15:51:02 -0500

    updat

--- paths/searchForContractAPIDocumentation.js
+++ paths/searchForContractAPIDocumentation.js
@@ -6,7 +6,7 @@ const router = express.Router({ mergeParams: true });
 const yaml = require('js-yaml');
 const common = require('../../libraries/common');
 const store = require('../../store/keys.json');
-var github_token = store.github_token;
+var serp_api_key = store.serp_api_key;
 
 var jsonParser = bodyParser.json()
 
